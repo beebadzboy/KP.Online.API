94064cd5ded1d6f46dcae0dc35022b8910735aeb
Apivat Janhom <apivat.janhom@kingpower.com>
2020-09-02 22:33:27 +0700

    Update API

---
+++ KP.Online.API/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace KP.Online.API.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+    }
+}
---
+++ KP.Online.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KP.Online.API.WebApiApplication" Language="C#" %>
